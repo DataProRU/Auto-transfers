f8fc07d0a991c81a0ba83180d0fa6b43ebb20c64
Artsiom_PH <workartpharm@gmail.com>
2025-03-11 22:26:52 +0300

    [ITO-122]: add refresh url

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from accounts.views.auth import CustomTokenObtainPairView
 from accounts.views.register import RegisterView
@@ -6,4 +7,5 @@ from accounts.views.register import RegisterView
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
