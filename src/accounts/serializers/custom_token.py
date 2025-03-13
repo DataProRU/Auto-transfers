3c48cf544fb68633e4c94437afbb911715605753
Artsiom_PH <workartpharm@gmail.com>
2025-03-13 13:13:40 +0300

    [ITO-122]: token req

--- src/accounts/serializers/custom_token.py
+++ src/accounts/serializers/custom_token.py
@@ -6,5 +6,12 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class CustomTokenSerializer(TokenObtainPairSerializer):
     def validate(self, attrs: dict[str, Any]) -> dict[str, Any]:
         data = super().validate(attrs)
-        data.update({"user_id": self.user.id, "role": self.user.role})
+        data.update(
+            {
+                "user_id": self.user.id,
+                "role": self.user.role,
+                "approved": self.user.is_approved,
+                "onboarded": self.user.is_onboarded,
+            }
+        )
         return data
