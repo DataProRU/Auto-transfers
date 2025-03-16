0d0aaa2b32f2f347bf3e488fe7978fe2fdb2fa18
Artsiom_PH <workartpharm@gmail.com>
2025-03-16 15:57:28 +0300

    [ITO-122]: added get documents

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,10 +4,11 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 from accounts.views.auth import CustomTokenObtainPairView
 from accounts.views.register import RegisterView
-from accounts.views.user import CurrentUserViewSet
+from accounts.views.user import CurrentUserViewSet, DocumentImageViewSet
 
 router = DefaultRouter()
-router.register(r"current-user", CurrentUserViewSet, basename="current_user")
+router.register(r"users/current-user", CurrentUserViewSet, basename="current_user")
+router.register(r"users/(?P<user_id>\d+)/documents", DocumentImageViewSet, basename="document_image")
 
 urlpatterns = [
     path("", include(router.urls)),
--- src/accounts/views/user.py
+++ src/accounts/views/user.py
@@ -1,17 +1,27 @@
 from typing import cast
 
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from accounts.models.user import User
-from accounts.serializers.user import UserSerializer
+from accounts.models.user import DocumentImage, User
+from accounts.serializers.user import DocumentImageSerializer, UserSerializer
 from accounts.services.single_resource import SingleResourceMixin
+from project.permissions import IsAdminOrManager
 
 
-class CurrentUserViewSet(SingleResourceMixin, viewsets.ModelViewSet):
+class CurrentUserViewSet(SingleResourceMixin, mixins.ListModelMixin):
     serializer_class = UserSerializer
     queryset = User.objects.order_by("id")
     permission_classes = [IsAuthenticated]
 
     def get_object(self) -> User:
         return cast(User, self.request.user)
+
+
+class DocumentImageViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = DocumentImageSerializer
+    permission_classes = [IsAdminOrManager]
+
+    def get_queryset(self) -> DocumentImage:
+        user_id = self.kwargs.get("user_id")
+        return DocumentImage.objects.filter(user_id=user_id)
