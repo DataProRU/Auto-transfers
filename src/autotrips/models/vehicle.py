f529b9dc2ec7ef42a2ab2ddce00bbb667a8a6b21
Artsiom_PH <workartpharm@gmail.com>
2025-04-28 12:08:06 +0300

    [ITO-201]: added vehicle model

---
+++ src/autotrips/models/vehicle.py
@@ -0,0 +1,41 @@
+import re
+from typing import cast
+
+from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils import timezone
+
+User = get_user_model()
+
+
+class VehicleType(models.Model):
+    v_type = models.CharField(max_length=100, default="SUV", null=False, blank=False)
+
+    def __str__(self) -> str:
+        return cast(str, self.v_type)
+
+
+class VehicleInfo(models.Model):
+    class Statuses(models.TextChoices):
+        NEW = "Новый"
+
+    client = models.ForeignKey(User, on_delete=models.RESTRICT, related_name="vehicles", null=False, blank=False)
+    brand = models.CharField(max_length=100, null=False, blank=False)
+    model = models.CharField(max_length=100, null=False, blank=False)
+    v_type = models.ForeignKey(
+        VehicleType, on_delete=models.SET_DEFAULT, related_name="vehicles", null=False, blank=False
+    )
+    vin = models.CharField(
+        validators=[RegexValidator(regex=re.compile(r"^[A-HJ-NPR-Z0-9]{17}$"), message="Invalid vin format.")]
+    )
+    container_number = models.CharField(max_length=100, null=False, blank=False)
+    arrival_date = models.DateField(null=False, blank=False)
+    transporter = models.CharField(max_length=100, null=False, blank=False)
+    recipient = models.CharField(max_length=100, null=False, blank=False)
+    comment = models.CharField(max_length=255, default="", blank=True)
+    status = models.CharField(max_length=10, choices=Statuses.choices, default=Statuses.NEW)
+    creation_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return f"{self.client.full_name}_{self.model}_{self.v_type}"
