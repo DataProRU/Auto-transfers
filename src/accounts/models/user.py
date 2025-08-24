3c0d9be1d938821eda93ac5d8b3eae5283d29bd2
Artsiom_PH <workartpharm@gmail.com>
2025-08-24 16:04:41 +0300

    [post_stage_2]: added translate to the user model

--- src/accounts/models/user.py
+++ src/accounts/models/user.py
@@ -9,22 +9,22 @@ from .managers import CustomUserManager
 
 class User(AbstractUser):
     class Roles(models.TextChoices):
-        ADMIN = "admin"
-        MANAGER = "manager"
-        USER = "user"
-        CLIENT = "client"
-        LOGISTICIAN = "logistician"
-        OPENING_MANAGER = "opening_manager"
-        TITLE = "title"
-        INSPECTOR = "inspector"
-        RE_EXPORT = "re_export"
+        ADMIN = "admin", _("Admin")
+        MANAGER = "manager", _("Manager")
+        USER = "user", _("User")
+        CLIENT = "client", _("Client")
+        LOGISTICIAN = "logistician", _("Logistician")
+        OPENING_MANAGER = "opening_manager", _("Opening manager")
+        TITLE = "title", _("Title")
+        INSPECTOR = "inspector", _("Inspector")
+        RE_EXPORT = "re_export", _("Re-Export")
 
     objects = CustomUserManager()
 
     username_validator = UnicodeUsernameValidator()
 
     username = models.CharField(
-        _("username"),
+        _("Username"),
         max_length=150,
         unique=True,
         help_text=_("Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."),
@@ -35,15 +35,15 @@ class User(AbstractUser):
         null=True,
         blank=True,
     )
-    full_name = models.CharField(max_length=255, null=False, blank=False)
-    phone = models.CharField(max_length=15, unique=True, null=False, blank=False)
-    telegram = models.CharField(max_length=32, unique=True, null=True, blank=True)
-    address = models.CharField(max_length=255, null=True, blank=True)  # noqa: DJ001
-    company = models.CharField(max_length=255, null=True, blank=True)  # noqa: DJ001
-    tg_user_id = models.BigIntegerField(unique=True, null=True, blank=True)
-    role = models.CharField(max_length=15, choices=Roles.choices, default=Roles.USER)
-    is_approved = models.BooleanField(default=False)
-    is_onboarded = models.BooleanField(default=False)
+    full_name = models.CharField(_("Full name"), max_length=255, null=False, blank=False)
+    phone = models.CharField(_("Phone"), max_length=15, unique=True, null=False, blank=False)
+    telegram = models.CharField(_("Telegram"), max_length=32, unique=True, null=True, blank=True)
+    address = models.CharField(_("Address"), max_length=255, null=True, blank=True)  # noqa: DJ001
+    company = models.CharField(_("Company"), max_length=255, null=True, blank=True)  # noqa: DJ001
+    tg_user_id = models.BigIntegerField(_("Telegram user ID"), unique=True, null=True, blank=True)
+    role = models.CharField(_("Role"), max_length=15, choices=Roles.choices, default=Roles.USER)
+    is_approved = models.BooleanField(_("Is approved"), default=False)
+    is_onboarded = models.BooleanField(_("Is onboarded"), default=False)
 
     USERNAME_FIELD = "phone"  # auth using phone
     REQUIRED_FIELDS: list[str] = ["username"]
@@ -53,9 +53,9 @@ class User(AbstractUser):
 
 
 class DocumentImage(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="documents")
-    image = models.ImageField(upload_to="documents/%Y/%m/%d/")
-    created = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="documents", verbose_name=_("User"))
+    image = models.ImageField(_("Image"), upload_to="documents/%Y/%m/%d/")
+    created = models.DateTimeField(_("Created"), default=timezone.now)
 
     def __str__(self) -> str:
         return f"{self.user.full_name}_image_{self.id}"
