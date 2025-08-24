863bbb0630660858136d52c46d2e5857230d985e
Artsiom_PH <workartpharm@gmail.com>
2025-08-24 20:40:39 +0300

    [post_stage_2]: added app translates

--- src/autotrips/apps.py
+++ src/autotrips/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class AutotripsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "autotrips"
+    verbose_name = _("Autotrips")
 
     def ready(self) -> None:
         import autotrips.signals  # noqa: F401
