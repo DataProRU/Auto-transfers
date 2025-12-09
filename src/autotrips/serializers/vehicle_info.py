0615ba1b4334036ee72e2c114e1755fbd2a2061d
Artsiom_PH <workartpharm@gmail.com>
2025-12-09 10:32:41 +0300

    change translates

--- src/autotrips/serializers/vehicle_info.py
+++ src/autotrips/serializers/vehicle_info.py
@@ -8,6 +8,7 @@ from django.core.files.uploadedfile import UploadedFile
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
+from django.utils.translation import gettext_lazy as _
 
 from accounts.serializers.custom_image import HEIFImageField
 from accounts.serializers.user import ClientSerializer
